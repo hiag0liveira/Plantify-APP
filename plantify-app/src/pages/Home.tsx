497d7d7bbb4e71b0e8d82edd313efbbcdbe059cf
Hiag0liveira <hiagophd@gmail.com>
2024-09-12 10:51:13 -0300

    fix: session one background text

--- plantify-app/src/pages/Home.tsx
+++ plantify-app/src/pages/Home.tsx
@@ -20,7 +20,6 @@ const Home: FC = () => {
 		triggerOnce: true,
 		threshold: 0.1,
 	})
-
 	return (
 		<div>
 			<div
@@ -29,24 +28,21 @@ const Home: FC = () => {
 					backgroundImage: `url(${imagem1})`,
 				}}
 			>
-				{/* Texto e botão sobrepostos */}
-				<div
-					className="absolute inset-0 flex flex-col justify-center items-start p-8 md:p-16 "
-					style={{
-						marginTop: '20%',
-					}}
-				>
-					<h4 className="text-white text-4xl md:text-5xl font-light leading-tight">
-						SUA LAVOURA <span className="font-bold italic">DIGITAL</span>
-						<br />
-						SUA GESTÃO <span className="font-bold italic">FACILITADA</span>
-					</h4>
-					<button className="mt-6 px-6 py-2 bg-orange-500 text-white text-lg rounded-md hover:bg-orange-600 transition duration-300">
-						Saiba mais
-					</button>
+				<div className="max-w-7xl">
+					{/* Texto e botão sobrepostos */}
+					<div className="inset-0 flex flex-col justify-center items-start absolute md:pt-36 md:pl-16">
+						<h4 className="text-white text-4xl md:text-5xl font-light leading-tight">
+							SUA LAVOURA <span className="font-semibold italic">DIGITAL</span>
+							<br />
+							SUA GESTÃO{' '}
+							<span className="font-semibold italic">FACILITADA</span>
+						</h4>
+						<button className="mt-6 px-6 py-2 bg-orange-500 text-white text-lg rounded-md hover:bg-orange-600 transition duration-300">
+							Saiba mais
+						</button>
+					</div>
 				</div>
 			</div>
-
 			<div className="text-center py-14 max-w-7xl  mx-auto ">
 				<h2 className="text-4xl font-bold text-green-600 mb-8">
 					Vantagens de ser Plantify
