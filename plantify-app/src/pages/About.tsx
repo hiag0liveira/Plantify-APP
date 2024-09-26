6c497c9df19d2649483ad7ac5d4f8e6da2e6c8b5
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-09-26 15:12:18 -0300

    feat: about

--- plantify-app/src/pages/About.tsx
+++ plantify-app/src/pages/About.tsx
@@ -1,9 +1,110 @@
 import { FC } from 'react'
+import fundo from '../assets/about/fotofundoquemsomos.png'
+import plantify from '../assets/logos/Plantify LOGO white.svg'
+import missao from '../assets/about/missao.png'
+import visao from '../assets/about/visao.png'
+import valores from '../assets/about/valores.png'
+import galera from '../assets/about/galera.png'
+import { IoCheckmarkCircle } from "react-icons/io5";
 
 const About: FC = () => {
 	return (
-		<div>
-			<h1>About!</h1>
+		<div className="relative w-full h-auto text-white">
+			{/* Seção da Imagem com Título */}
+			<div
+				className="relative bg-cover bg-center h-64 sm:h-80 md:h-96"
+				style={{ backgroundImage: `url(${fundo})` }}
+			>
+				<div className="absolute inset-0 bg-black bg-opacity-10"></div>
+				<div className="relative z-10 flex flex-col items-center justify-center h-full text-center">
+					<img src={plantify} />
+					<p className="italic text-2xl sm:text-3xl md:text-4xl">
+						Cultivando Tecnologia, Colhendo Resultados
+					</p>
+				</div>
+			</div>
+
+			{/* Seção do Texto Informativo */}
+			<div className="p-8 bg-greenFigman">
+				<p className="text-white text-lg sm:text-xl md:text-2xl leading-relaxed text-center whitespace-normal">
+					A Plantify Soluções é um projeto de empreendedorismo criado por estudantes <br />
+					para ajudar pequenos agricultores a gerenciar suas propriedades de forma <br />
+					eficiente e moderna. O aplicativo oferece monitoramento de safra, previsão <br />
+					climática, controle de insumos e venda de produtos, tudo voltado para <br />
+					dispositivos de baixo custo e com pouca internet. O objetivo é democratizar o <br />
+					acesso à tecnologia, melhorando a produtividade e promovendo a <br />
+					sustentabilidade entre os agricultores.
+				</p>
+			</div>
+			<div>
+				<div className='flex mt-12 ml-12 mb-12 items-center'>
+					<div className='mr-8 ml-8'>
+						<h1 className='text-orange-500 text-4xl mb-6 font-bold'>Nossa Missão</h1>
+						<p className='text-black text-2xl'>
+							Simplificar e otimizar a vida dos <br />
+							pequenos e médios produtores <br />
+							rurais, fornecendo ferramentas <br />
+							tecnológicas que tomam a gestão <br />
+							da produção agrícola mais <br />
+							eficiente, sustentável e lucrativa.
+						</p>
+					</div>
+					<div className='ml-44'>
+						<img src={missao} className='w-auto' />
+					</div>
+				</div>
+
+				<div className='flex mt-12 ml-12 mb-12 items-center'>
+					<div className=''>
+						<img src={visao} className='w-auto' />
+					</div>
+					<div className='ml-48'>
+						<h1 className='text-orange-500 text-4xl mb-6 font-bold text-right'>Nossa Visào</h1>
+						<p className='text-black text-2xl text-right'>
+							Revolucionar o modo como a <br />
+							agricultura é gerida, tornando a <br />
+							Plantify a referência em tecnologia <br />
+							e sustentabilidade, permitindo que <br />
+							os produtores melhorem suas <br />
+							colheitas e contruam uma rede <br />
+							de colaboracao ativa.
+						</p>
+					</div>
+				</div>
+
+				<div className='flex mt-12 ml-12 mb-12 items-center'>
+					<div className='mr-8 ml-8'>
+						<h1 className='text-orange-500 text-4xl mb-6 font-bold'>Nossos Valores</h1>
+						<p className='text-black text-2xl flex items-center'>
+							<IoCheckmarkCircle className='mr-2 text-green-500' />  Inovaçào
+						</p>
+						<p className='text-black text-2xl flex items-center'>
+							<IoCheckmarkCircle className='mr-2 text-green-500' />	Colaboraçào
+						</p>
+						<p className='text-black text-2xl flex items-center'>
+							<IoCheckmarkCircle className='mr-2 text-green-500' />	transparência
+						</p>
+						<p className='text-black text-2xl flex items-center'>
+							<IoCheckmarkCircle className='mr-2 text-green-500' />	Sustentabilidade
+						</p>
+						<p className='text-black text-2xl flex items-center'>
+							<IoCheckmarkCircle className='mr-2 text-green-500' />	Excelência
+						</p>
+					</div>
+					<div className='ml-64'>
+						<img src={valores} className='w-auto' />
+					</div>
+				</div>
+
+				<div>
+					<img src={galera} />
+				</div>
+				<div className='flex justify-center mt-10 mb-10'>
+					<button className='bg-white border border-orange-500 text-orange-500 border-4 py-3 px-6 text-3xl font-bold rounded-lg hover:bg-orange-500 hover:text-white transition-colors duration-300'>
+						Seja um parceiro Plantify
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
